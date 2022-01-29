ea926d3fa5b7fa0e16b121bb6da0a7d82508baf8
dubiniak <xdubiniakpl@gmail.com>
2022-01-29 17:53:46 +0100

    CSS_DAMIAN_D

---
+++ WypozyczalniaSprzetuKomputerowego/Data/Migrations/20220129164344_calete2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WypozyczalniaSprzetuKomputerowego.Data.Migrations
+{
+    public partial class calete2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
